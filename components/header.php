5f21fd8596887fbb0ba8defa631043883eaf8271
abdenassar01 <abdenassaramimi99@gmail.com>
2023-03-29 01:42:49 +0000

    fix logo appearance🤯🤚

--- components/header.php
+++ components/header.php
@@ -1,7 +1,7 @@
-<div class="navbar navbar-expand-lg bg-body-tertiary" style="min-height: 20vh">
+<div class="navbar navbar-expand-lg bg-body-tertiary" style="min-height: 10vh">
     <div class="container-fluid">
         <a class="navbar-brand" href="../index.php">
-            <img src="https://i.imgur.com/w4BIv7G.png" alt="logo cameroon events" width="100" height="auto">
+            <img src="https://i.imgur.com/jUBeM4g.png" alt="logo cameroon events" width="100" height="auto">
         </a>
         <div class="d-flex">
             <a href="./pages/new_event" class="btn btn-primary"><i class="fa fa-plus-circle" aria-hidden="true"></i> add event</a>
