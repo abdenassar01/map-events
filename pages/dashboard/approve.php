5a70bbac99de4462af2128910b8aa8e1a51d3587
abdenassar01 <abdenassaramimi99@gmail.com>
2023-04-06 15:35:49 +0000

    add approve handler to approve events by admin💪👏

--- pages/dashboard/approve.php
+++ pages/dashboard/approve.php
@@ -1,1 +1,13 @@
 <?php
+include("../../config/db.php");
+if (!empty($db)) {
+    $sql = "UPDATE event SET status = 'approved' WHERE id = :id;";
+    $statement = $db->prepare($sql);
+    $statement->bindParam(":id", $_GET['id']);
+
+    if ($statement->execute()){
+        header('Location: ./');
+    }else{
+        echo "<script>alert('Something went wrong')</script>";
+    }
+}
