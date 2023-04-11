322d669d274d81832a78a882a5cf8f94756cb5c6
abdenassar01 <abdenassaramimi99@gmail.com>
2023-04-11 14:58:48 +0000

    fix a typo in count endpoints🧐😒

--- api/events/count/all/index.php
+++ api/events/count/all/index.php
@@ -3,7 +3,7 @@
     include("../../../../config/db.php");
     if(!empty($db)){
 
-        $req = "select departement_id as 'departement', count(id) as 'nbr_events' from event where and status = 'approved' group by departement_id;";
+        $req = "select departement_id as 'departement', count(id) as 'nbr_events' from event where status = 'approved' group by departement_id;";
         $st = $db->prepare($req);
         if($st->execute()){
             echo json_encode($st->fetchAll(PDO::FETCH_ASSOC));
