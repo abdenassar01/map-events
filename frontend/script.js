93d199f2af8370a78bc44f6e8ca06d9583f533b8
abdenassar01 <abdenassaramimi99@gmail.com>
2023-04-12 23:36:31 +0000

    minor fix (remove unused variable) 🤛🤗

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,6 @@
 let map = L.map('map').setView([5.694, 12.742], 7);
 
-let tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
