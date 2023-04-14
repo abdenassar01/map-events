ffea0f72f627d4ad5d2de1f0bc55191be0800ff2
abdenassar01 <abdenassaramimi99@gmail.com>
2023-04-14 22:54:27 +0000

    add delete page😒🖖

---
+++ pages/dashboard/user/delete.php
@@ -0,0 +1,14 @@
+<?php
+
+include("../../../config/db.php");
+if (!empty($db)) {
+    $sql = "DELETE FROM user WHERE id = :id;";
+    $statement = $db->prepare($sql);
+    $statement->bindParam(":id", $_GET['id']);
+
+    if ($statement->execute()) {
+        header('Location: ./');
+    } else {
+        echo "<script>alert('Something went wrong')</script>";
+    }
+}
