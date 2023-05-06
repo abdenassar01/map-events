4ae98a7fa3e777217c73e79093de44947366251a
abdenassar <abdenassaramimi99@gmail.com>
2023-05-06 11:04:48 +0100

    fix latest events endpoint, desc instead of asc

--- api/events/latest/index.php
+++ api/events/latest/index.php
@@ -3,7 +3,7 @@
 include("../../../config/db.php");
 
 if(!empty($db)){
-    $req = "SELECT * FROM event ORDER BY date_created LIMIT 20;";
+    $req = "SELECT * FROM event ORDER BY date_created DESC LIMIT 20;";
     $st = $db->prepare($req);
     if($st->execute()){
         echo json_encode($st->fetchAll(PDO::FETCH_ASSOC));
